b4b62ce2888433215688942d37675937b2e7ec6a
alayek <5607371+alayek@users.noreply.github.com>
2025-08-01 21:24:18 +0530

    chore: vendor chunking

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    minify: "terser",
+    rollupOptions: {
+      output: {
+        manualChunks: (id: string) =>
+          id.includes("node_modules") ? "vendor" : null,
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
