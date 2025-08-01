c83f6dff335c446465c3f810621dc7ff5821ea6f
Arijit Layek <5607371+alayek@users.noreply.github.com>
2025-08-01 21:35:43 +0530

    Setup Vercel Preview and Organize Assets (#2)
    * feat: setup tailwind and organize assets
    
    * chore: fix build
    
    * chore: vendor chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    minify: "terser",
+    rollupOptions: {
+      output: {
+        manualChunks: (id: string) =>
+          id.includes("node_modules") ? "vendor" : null,
+      },
+    },
+  },
+  plugins: [react(), tailwindcss()],
 });
