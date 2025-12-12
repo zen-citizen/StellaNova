af9a33beb30b1a4e646cd74c8a9e1cfc9913671a
abhik <abhiknvarma@gmail.com>
2025-12-12 21:05:38 +0530

    add bbmp ward numbers back

--- backend/internal/cities/bengaluru.go
+++ backend/internal/cities/bengaluru.go
@@ -91,12 +91,13 @@ func (p *BengaluruProvider) getGBAEntity(ctx context.Context, lat, lng float64) 
 
 func (p *BengaluruProvider) getBBMPEntity(ctx context.Context, lat, lng float64) *models.Entity {
 	attributes := utils.ExtractAttributes(ctx, p.geoManager, lat, lng, p.Name(), "bbmp", func(props map[string]interface{}) ([]models.Attribute, error) {
+		wardNumber := getStringAttribute(props, "Ward Number", "ward_number", "")
 		ward := getStringAttribute(props, "Ward Name", "ward_name", "")
 		zone := getStringAttribute(props, "Zone", "zone", "")
 		division := getStringAttribute(props, "Division", "division", "")
 		subdivision := getStringAttribute(props, "Subdivision", "subdivision", "")
 
-		return []models.Attribute{*ward, *zone, *division, *subdivision}, nil
+		return []models.Attribute{*wardNumber, *ward, *zone, *division, *subdivision}, nil
 	}, p.logger)
 
 	disclaimer := "This information is based on the 198-ward classification, which BBMP used as a reference until the GBA was formed."
