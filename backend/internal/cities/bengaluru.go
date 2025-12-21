3e470d08e28d38592f104d7251ce789ff706eb56
abhik <abhiknvarma@gmail.com>
2025-12-21 22:45:06 +0530

    add constituency names to bengaluru

--- backend/internal/cities/bengaluru.go
+++ backend/internal/cities/bengaluru.go
@@ -72,6 +72,10 @@ func (p *BengaluruProvider) GetEntities(ctx context.Context, lat, lng float64) (
 		entities = append(entities, *entity)
 	}
 
+	if entity := p.getConstituencyEntity(ctx, lat, lng); entity != nil {
+		entities = append(entities, *entity)
+	}
+
 	return entities, nil
 }
 
@@ -338,4 +342,17 @@ func getAddressAttribute(data map[string]interface{}, name, key, valueDefault, a
 
 	attribute.Address = &address
 	return attribute
+}
+
+func (p *BengaluruProvider) getConstituencyEntity(ctx context.Context, lat, lng float64) *models.Entity {
+	attributes := append(
+		utils.ExtractAttributes(ctx, p.geoManager, lat, lng, p.Name(), "constituency", nil, p.logger),
+	)
+
+	entity := utils.BuildEntity(ctx, "Constituency",
+		"This information is unavailable for this address. This could be because the area is outside Bengaluru Constituency limits.",
+		nil,
+		attributes, p.logger)
+
+	return &entity
 }
